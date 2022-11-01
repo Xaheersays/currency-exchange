9dddc6e878d2917ce1599ad9808ab82b44564695
Zaheer <105771794+Xaheersays@users.noreply.github.com>
2022-11-01 23:33:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ document.addEventListener("DOMContentLoaded",()=>{
 
     document.querySelector("#currency-converter").addEventListener("submit",(event)=>{
         event.preventDefault();
-
         const {target:{from,to,amount}}=event;   
-
         var headers = new Headers();
         headers.append("apikey", "MKcKzKYUvbYuTFW0lQFoQVKjhCXjP49F");
         
@@ -12,9 +10,6 @@ document.addEventListener("DOMContentLoaded",()=>{
           method: 'GET',
           headers
         };
-
-        // {"success":true,"query":{"from":"USD","to":"INR","amount":1},"info":{"timestamp":1667324103,"rate":82.605969},"date": "2022-11-01","result":82.605969}
-        
 
         fetch(`https://api.apilayer.com/exchangerates_data/convert?to=${to.value}&from=${from.value}&amount=${amount.valueAsNumber}`, requestOptions)
         .then(response => response.json())
